065f0d68f3ac77770c5e4bd1696fe43b01cd12f1
JoaZabala <joaquinzabala118@gmail.com>
2023-11-07 00:18:09 -0300

    helper , datos

---
+++ Automotriz/AutomotrizBack/Datos/HelperDB.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotrizBack.Datos
+{
+    public class HelperDB
+    {
+        SqlConnection conexion;
+        static HelperDB instancia;
+        public HelperDB()
+        {
+          conexion = new SqlConnection();
+        }
+
+        public static HelperDB GetInstancia()
+        {
+            if(instancia == null)
+            {
+                instancia = new HelperDB();
+            }
+            return instancia;
+        }
+
+        public DataTable Consulta(string nombreSP)
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(nombreSP,conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable tabla = new DataTable();
+            tabla.Load(cmd.ExecuteReader());
+            conexion.Close();
+            return tabla;
+        }
+
+        public DataTable ConsultaParametros(string nombreSp , List<Parametro>parametros)
+        {
+            conexion.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = nombreSp;
+            comando.Parameters.Clear();
+            foreach (Parametro p in parametros)
+            {
+                comando.Parameters.AddWithValue(p.Nombre, p.Valor);
+            }
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+            conexion.Close();
+            return tabla;
+        }
+        public SqlConnection GetConexion()
+        {
+            return conexion;
+        }
+
+    }
+}
