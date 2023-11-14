26e07cb7077df411ecb9e718b36fdd41967b4aff
Average-Guy0 <alejandroziade@gmail.com>
2023-11-13 23:34:05 -0300

    checkeando un problema de sofi, ya vuelvo

---
+++ Automotriz/AutomotrizBack/Entidades/TipoContacto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotrizBack.Entidades
+{
+    public class TipoContacto
+    {
+        public int cod { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public TipoContacto()
+        {
+            cod = 0;
+            Descripcion = string.Empty;
+        }
+    }
+}
